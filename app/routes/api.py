6c8f1aaa8cd4468bbe2a44e713f2ca25eda482c0
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-09-13 09:21:01 +0200

    fix: don't import missing router

--- app/routes/api.py
+++ app/routes/api.py
@@ -4,9 +4,9 @@ API redirection router
 
 from fastapi import APIRouter
 
-from app.routes import vocabs, autocomplete, search
+from app.routes import vocabs, search
 
 router = APIRouter()
-router.include_router(autocomplete.router, tags=["autocomplete"], prefix="/autocomplete")
+# router.include_router(autocomplete.router, tags=["autocomplete"], prefix="/autocomplete")
 router.include_router(vocabs.router, tags=["vocabs"], prefix="/vocabs")
 router.include_router(search.router, tags=["search"], prefix="/search")
